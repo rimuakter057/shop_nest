2523aae4bcce08c48ee108dc1a8135d3240b544e
rimuakter057 <rimu.appdeveloper@gmail.com>
2025-02-26 16:03:09 +0600

    real

--- lib/features/authentication/screens/team_list_screen/team_list_screen.dart
+++ lib/features/authentication/screens/team_list_screen/team_list_screen.dart
@@ -49,54 +49,57 @@ _footballScoreList.add(FootballScore.fromJson(doc.data(), doc.id));
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:/*Visibility(
-        visible: _getInProgress==false,
-        replacement: Center(child: CircularProgressIndicator(),),
-        child: ListView.builder(
-          itemCount: _footballScoreList.length,
-          itemBuilder: (context,index){
-            FootballScore score = _footballScoreList[index];
-          return ListTile(
-            title: Text(score.team1Name),
-            subtitle: Text(score.team1Score.toString()),
-            trailing: Text(score.team2Score.toString()),
-          );
-        },
-        ),
-      ),*/
-
-       Visibility(
-        visible: _getInProgress == false,
-        replacement: const Center(
-          child: CircularProgressIndicator(),
-        ),
-        child: ListView.builder(
-          itemCount: _footballScoreList.length,
-          itemBuilder: (context, index) {
-            FootballScore score = _footballScoreList[index];
-            return ListTile(
-              title: Text(score.matchName),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('${score.team1Name} vs ${score.team2Name}'),
-                  if (score.isRunning == false)
-                    Text('Winner team is ${score.winnerTeam}')
-                ],
-              ),
-              trailing: Text(
-                '${score.team1Score}-${score.team2Score}',
-                style:
-                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              leading: CircleAvatar(
-                backgroundColor: score.isRunning ? Colors.green : Colors.grey,
-                radius: 10,
-              ),
-            );
-          },
-        ),
+      body:  StreamBuilder(
+        stream: bd.collection('Football').snapshots(),
+        builder: (context,AsyncSnapshot<QuerySnapshot> snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return const Center(child: CircularProgressIndicator(),);
+          }
+          if(snapshot.hasError){
+            return Center(child: Text(snapshot.error.toString()),);
+          }
+         if(snapshot.hasData){
+            QuerySnapshot<Map<String,dynamic>> querySnapshot = snapshot.data! as QuerySnapshot <Map<String,dynamic>> ;
+       _preparedScoreList(querySnapshot);
+           return ListView.builder(
+             itemCount: _footballScoreList.length,
+             itemBuilder: (context, index) {
+               FootballScore score = _footballScoreList[index];
+               return ListTile(
+                 title: Text(score.matchName),
+                 subtitle: Column(
+                   crossAxisAlignment: CrossAxisAlignment.start,
+                   children: [
+                     Text('${score.team1Name} vs ${score.team2Name}'),
+                     if (score.isRunning == false)
+                       Text('Winner team is ${score.winnerTeam}')
+                   ],
+                 ),
+                 trailing: Text(
+                   '${score.team1Score}-${score.team2Score}',
+                   style:
+                   const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                 ),
+                 leading: CircleAvatar(
+                   backgroundColor: score.isRunning ? Colors.green : Colors.grey,
+                   radius: 10,
+                 ),
+               );
+             },
+           );
+         }
+          return const SizedBox();
+        }
       ),
     );
   }
+  void _preparedScoreList( QuerySnapshot<Map<String,dynamic>> querySnapshot){
+    _footballScoreList.clear();
+    for (QueryDocumentSnapshot<Map<String,dynamic>> doc in querySnapshot.docs){
+      _footballScoreList.add(FootballScore.fromJson(doc.data(), doc.id));
+    }
+
+  }
+
+
 }
